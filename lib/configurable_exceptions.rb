2a64398fff144997553cfb90b3a203631127bd8f
Qihuan Piao <fantasyday@gmail.com>
2017-03-22 17:21:06 +0900

    Can't set the default value with block syntax in Rails 5
    Related mattr_accessor change: https://github.com/rails/rails/pull/21158
    
    And now Rails will turn a `nil` "action_dispatch.show_exceptions" to `true` for show_exceptions?, that leads to unexpected behavior for the gem.
    https://github.com/rails/rails/blob/afbbcf24d791362a65c7f29b739c73a5b09e3816/actionpack/lib/action_dispatch/http/request.rb#L166-L171

--- lib/configurable_exceptions.rb
+++ lib/configurable_exceptions.rb
@@ -4,8 +4,7 @@ require 'configurable_exceptions/rails' if defined? Rails::Railtie
 
 module ConfigurableExceptions
   class << self
-    cattr_accessor :show_exceptions do
-      false
-    end
+    cattr_accessor :show_exceptions
+    self.show_exceptions = false
   end
 end
